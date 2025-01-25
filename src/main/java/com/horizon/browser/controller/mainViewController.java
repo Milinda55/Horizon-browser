e146c36dd91f853aad26977e676cde22265a9c18
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-01-26 03:25:38 +0530

    Set default url to the text field and add a listner

--- src/main/java/com/horizon/browser/controller/mainViewController.java
+++ src/main/java/com/horizon/browser/controller/mainViewController.java
@@ -1,4 +1,23 @@
 package com.horizon.browser.controller;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebView;
+
 public class mainViewController {
+    public AnchorPane root;
+    public WebView wbView;
+    public TextField txtAddress;
+
+    public void initialize() throws Exception {
+        txtAddress.setText("http://ikman.lk/");
+        txtAddress.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) Platform.runLater(txtAddress::selectAll);
+        });
+    }
+
+    public void txtAddressOnAction(ActionEvent actionEvent) {
+    }
 }
