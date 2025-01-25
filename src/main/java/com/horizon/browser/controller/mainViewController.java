a6c99eb01f48faa51b34653063dade77ec96544c
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-01-26 03:34:28 +0530

    Create a method to get the protocol using substring

--- src/main/java/com/horizon/browser/controller/mainViewController.java
+++ src/main/java/com/horizon/browser/controller/mainViewController.java
@@ -6,6 +6,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.web.WebView;
 
+import java.io.IOException;
+
 public class mainViewController {
     public AnchorPane root;
     public WebView wbView;
@@ -13,13 +15,25 @@ public class mainViewController {
 
     public void initialize() throws Exception {
         txtAddress.setText("http://ikman.lk/");
+        loadWebPage(txtAddress.getText());
         txtAddress.focusedProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) Platform.runLater(txtAddress::selectAll);
         });
     }
 
-    public void txtAddressOnAction(ActionEvent actionEvent) {
+    public void txtAddressOnAction(ActionEvent actionEvent) throws IOException {
         String url = txtAddress.getText();
         if (url.isBlank()) return;
+        loadWebPage(url);
     }
+
+    private void loadWebPage(String url) throws IOException {
+
+        // protocol
+        int protocolEndIndex = url.indexOf("://");
+        String protocol = (protocolEndIndex != -1) ? url.substring(0, protocolEndIndex) : "http";
+    }
+
+
+
 }
